8adbf987978915ebe27a80cd40fce434a7cf940f
seanreed1111 <seanreed1111@gmail.com>
2016-04-12 14:38:10 -0400

    adds initial validation of Micropost model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
+  validates :content, presence:true
+  validates :user_id, presence:true
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  let(:newPost) {}
+  before :each do
+    @user = User.create(name: "Sean", email:"sean@sean.com")
+    
+    @user.microposts << Micropost.new(content: "fake content")
+  end
 
+  it "validates presence of content" do
+    @firstPost = @user.microposts.first
+    @firstPost.content = nil
+    @firstPost.valid?
+    expect(@firstPost.errors[:content]).to include "can't be blank"
+  end
+  it "belongs to a user" do
+    @firstPost = @user.microposts.first
+    @firstPost.user_id = nil
+    @firstPost.valid?
+    expect(@firstPost.errors[:user_id]).to include "can't be blank"
+
+  end
 end
